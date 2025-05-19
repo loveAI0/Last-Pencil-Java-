8998be9ce00a71fde10f4a71d3cccd936fbbe09d
loveAI0 <140394889+loveAI0@users.noreply.github.com>
2025-05-19 12:07:08 +0000

    Game is ready to play

---
+++ Game.java
@@ -0,0 +1,58 @@
+import java.util.Scanner;
+
+
+public class Game {
+    static void playGame(){
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("How many pencils would you like to use:");
+        int pencilNumber = input.nextInt();
+        input.nextLine();
+        String player1 = input.nextLine();
+            
+        String player2 = input.nextLine();
+
+        
+        System.out.println(String.format("Who will be the first (%s, %s):", player1, player2));
+        String playerToPlay = input.nextLine();
+
+        for(int i = 0 ; i < pencilNumber; i++){
+                System.out.print("|" + " ");
+        }
+        
+        
+        System.out.println();
+        /* 
+        System.out.println(String.format("%s is going first!", playerToPlay));
+        */
+       
+        boolean flag = true;
+        boolean toogleFlag = true;
+
+        while(flag){ 
+         
+
+         System.out.println(String.format("%s's turn:", playerToPlay));
+         
+         System.out.println("pencil removed");
+         int pencilRemoved = input.nextInt();
+         input.nextLine(); 
+
+         pencilNumber -= pencilRemoved;
+         if(pencilNumber == 0){
+            flag = false;
+         }
+          
+          for(int i = 0 ; i < pencilNumber ; i++){ 
+                System.out.print("|" + " ");
+          }
+          System.out.println();
+          playerToPlay = toogleFlag ? player2 : player1;
+          toogleFlag = !toogleFlag;
+          
+          
+        }   
+    }
+}
+    
+
--- Main.java
+++ Main.java
@@ -4,24 +4,9 @@ class Main{
         System.out.println("|||||||");
         System.out.println("Your turn!");
 
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("How many pencils would you like to use:");
-        int pencilNumber = input.nextInt();
-        input.nextLine();
-        String player1 = input.nextLine();
         
-        String player2 = input.nextLine();
-        System.out.println(String.format("Who will be the first (%s, %s):", player1, player2));
+       Game.playGame();
         
-        String playerToPlay = input.nextLine();
-
-        for(int i = 0 ; i < pencilNumber; i++){
-            System.out.print("|" + " ");
-        }
-        System.out.println();
-        System.out.println(String.format("%s is going first!", playerToPlay));
 
     }
 }
